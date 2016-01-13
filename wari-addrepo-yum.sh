879c4cba730c83efc0e7635fe299843f73c33002
Stuart Campbell <stuart@stuartcampbell.me>
2016-01-13 16:36:47 -0500

    changed order of logic for testing repo existance.
    Now we test for the existance of the .repo file inside the
    function to add the repo.

--- wari-addrepo-yum.sh
+++ wari-addrepo-yum.sh
@@ -1,3 +1,6 @@
+# Standard place to look for installed .repo files.
+REPO_DIR="/etc/yum.repos.d/"
+
 get_dnf () {
     DNF=`which yum`
     if [ $(command -v dnf) ]; then
@@ -6,65 +9,35 @@ get_dnf () {
     echo ${DNF}
 }
 
-add_repo_rpm() {
+add_yum_repo_rpm() {
     echo "sudo ${DNF} install $@"
     sudo ${DNF} install $@
 }
 
-add_repo_conf() {
+add_yum_repo_url() {
     URL=${1}
-    if [[ $DNF == *dnf ]]; then
+
+    # Extract the filename
+    REPOFILENAME=${URL##/*/}
+
+    # Determine if we are using dnf or yum
+    DNF=$(get_dnf)
+
+    # Check if the .repo file is already installed
+    if [ ! -f "$REPO_DIR/$REPOFILENAME" ]; then
+      if [[ $DNF == *dnf ]]; then
         CMD="${DNF} config-manager"
-    elif [[ $DNF == *yum ]]; then
+      elif [[ $DNF == *yum ]]; then
         CMD="yum-config-manager"
-    else
+      else
         echo "Something went wrong"
         exit 1
+      fi
+      echo "sudo ${CMD} --add-repo=${URL}"
+      sudo ${CMD} --add-repo=${URL}
+    else
+      # .repo file is already installed - do nothing
+      echo "Repofile $REPOFILENAME already exists"
     fi
-    echo "sudo ${CMD} --add-repo=${URL}"
-    sudo ${CMD} --add-repo=${URL}
 }
 
-DNF=$(get_dnf)
-REPO_DIR="/etc/yum.repos.d/"
-REPOS=$@
-
-# add the repositories that were requested
-echo "Setting up repositories: $REPOS"
-for ITEM in $REPOS
-do
-    if [ ! -f "$REPO_DIR/$ITEM.repo" ]; then
-        case "$ITEM" in
-            google-chrome)
-                echo "Get the google-chrome rpm from https://www.google.com/chrome/browser/desktop/index.html"
-                exit 1
-                ;;
-
-	    epel)
-                add_repo_rpm epel-release
-	        ;;
-
-            rpmfusion-free)
-                add_repo_rpm --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-                ;;
-
-            rpmfusion-nonfree)
-                add_repo_rpm --nogpgcheck http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-                ;;
-
-            fedora-nvidia)
-                if [ ! $(rpm -E %fedora) == "%fedora" ]; then
-                    add_repo_conf  http://negativo17.org/repos/fedora-nvidia.repo
-                elif [ ! $(rpm -E %rhel) == "%rhel" ]; then
-                    add_repo_conf http://negativo17.org/repos/epel-nvidia.repo
-                else
-                    echo "Do not have url for fedora-nvidia"
-                    exit 1
-                fi
-                ;;
-            *)
-                echo "Do not no how to install '$ITEM.repo'"
-                exit
-        esac
-    fi
-done
