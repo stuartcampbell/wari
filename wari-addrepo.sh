716a4871bdc62d332b912040542987e2ee248838
Pete Peterson <peterfpeterson@gmail.com>
2016-01-14 14:06:13 -0500

    Adding support for specifying multiple repositories at once

--- wari-addrepo.sh
+++ wari-addrepo.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 add_repo() {
-  REPO=${1}
-  if [ -f $WARI_ROOT/repos/$REPO ]; then
-    source $WARI_ROOT/repos/$REPO
-  else
-    echo "Could not find information about $REPO"
-    exit 1
-  fi
+  for REPO in ${@}
+  do
+    if [ -f $WARI_ROOT/repos/$REPO ]; then
+      source $WARI_ROOT/repos/$REPO
+    else
+      echo "Could not find information about $REPO"
+      exit 1
+    fi
+  done
 }
-
