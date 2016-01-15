cad2f75ce865dbf7e598dc8ff226c688247d343f
Stuart Campbell <stuart@stuartcampbell.me>
2016-01-15 14:43:48 -0500

    Get rid of bashism

--- wari-addrepo.sh
+++ wari-addrepo.sh
@@ -4,7 +4,7 @@ add_repo() {
   for REPO in ${@}
   do
     if [ -f $WARI_ROOT/repos/$REPO ]; then
-      source $WARI_ROOT/repos/$REPO
+      . $WARI_ROOT/repos/$REPO
     else
       echo "Could not find information about $REPO"
       exit 1
