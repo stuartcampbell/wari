806c0950b1fe66914d8e18e683ad2a4d0ac397b6
Stuart Campbell <stuart@stuartcampbell.me>
2016-01-15 14:48:38 -0500

    Check for WARI_ROOT

--- wari-setup.sh
+++ wari-setup.sh
@@ -1,17 +1,4 @@
-#!/bin/sh
-
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
-     # While $SOURCE is a symlink, resolve it
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-     echo "$DIR"
-}
+#! /bin/bash
 
 url_exists () {
   URL=${1}
@@ -26,10 +13,6 @@ url_exists () {
   fi
 }
 
-WARI_ROOT=$(get_script_dir)
-
-echo "Running wari from $WARI_ROOT"
-
 # Now let's determine what distro we are running on.
 if [ -f "/etc/os-release" ]; then
   . /etc/os-release
@@ -40,11 +23,19 @@ else
   exit -1
 fi
 
-if [[ "$(uname -m)" = "x86_64" ]]; then
+if [ "$(uname -m)" = "x86_64" ]; then
 	WARI_ARCH="64"
 else
 	WARI_ARCH="32"
 fi
+
+if [ -z "$WARI_ROOT" ]; then
+  echo "Please set WARI_ROOT"
+  exit 1
+fi
+
+echo "WARI_ROOT: $WARI_ROOT"
+echo "WARI_DISTRO: $WARI_DISTRO ($WARI_DISTRO_VERSION)"
 
 # Let's source all our helper libraries
 . $WARI_ROOT/wari-addrepo.sh
